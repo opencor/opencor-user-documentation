7e47a6bf06459a0484ebd1c8359dbcfc7c889fa4
Alan Garny <agarny@hellix.com>
2019-12-16 11:07:29 +1300

    Test to build our documentation on RTD.

--- src/conf.py
+++ src/conf.py
@@ -19,6 +19,21 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import datetime
+import os
+
+now = datetime.datetime.now()
+
+os.system(  'cd build \
+          && git clone https://github.com/opencor/sphinx-theme theme \
+          && cp ../src/conf.py . \
+          && echo "copyright = u\'2011-%d, University of Auckland\'\nhtml_theme = \'theme\'\nhtml_show_sphinx = False\nhtml_show_copyright = False" >> conf.py \
+          && sphinx-build -q -b html -c . ../src html \
+          && python theme/cmake/stringreplace.py html/_static/theme.css "103, 103, 103" "54, 96, 146" \
+          && python theme/cmake/appendfile.py html/_static/theme.css ../styles/opencor.css \
+          && python theme/cmake/appendfile.py html/_static/pygments.css ../styles/cellmlText.css'
+          % now.year)
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
